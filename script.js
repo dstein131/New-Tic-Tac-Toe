d112aa2b0ea02ff91a509da808295799328998a0
David Stein <dstein131@gmail.com>
2022-04-02 20:35:13 -0400

    working without win

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const gameState = {
     players: ['X', 'O'],
     currentTurn: [],
     playerName: ["", ""],
-    currentName: [""],
+    currentName: [],
     gameBoard: Array(9).fill(null)
 }
 
+var players = gameState.players
 var gameBoard = gameState.gameBoard
-var currentPlayer = gameState.currentTurn
+var currentTurn = gameState.currentTurn
 var currentName = gameState.currentName
+var playerNames = gameState.playerName
 var statusBar = document.getElementById("statusBar")
 
 function start() {
@@ -16,10 +18,11 @@ function start() {
     var clear = document.getElementById("hidder")
     clear.style.display = "none"
     wrap.style.display = "flex"
+    currentTurn = gameState.players[0]
     getNames()
     randomlyChooseName()
-    
     document.querySelectorAll('.cell').forEach(item => item.addEventListener('click', clicked))
+    console.log(currentTurn)
 }
 
 function getNames() {
@@ -40,10 +43,43 @@ function randomlyChooseName() {
     currentName = randomElement
     console.log (currentName)
     statusBar.innerText = `It's ${currentName}'s turn`
-
 }
 
 function clicked(e) {
-
+    markBox(e)
 }
 
+function markBox(e) {
+    let target = e.target
+    let marker = e.target.id
+    let inner = e.target.innerText
+    if (inner === "") {
+        target.innerText = currentTurn
+        gameBoard[marker] = currentTurn   
+        console.log(gameBoard)
+        swapName()
+        swapTurn()
+    }    
+}
+
+
+
+function swapTurn() {
+    if (currentTurn === players[0]) {
+        currentTurn = players[1]
+    }
+    else {
+        currentTurn = players[0]
+    }
+    console.log(currentTurn)
+}
+
+function swapName() {
+    if (currentName === playerNames[0]) {
+        currentName = playerNames[1]
+    }
+    else {
+        currentName = playerNames[0]
+    }
+    console.log(currentName)
+}
